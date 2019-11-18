ca41090f7ae2b358d424bacfcc099925da5cb8fa
Thomas C. Brewer <tcb93@ice.cs.rutgers.edu>
2019-11-18 17:25:17 -0500

    Added insightful comments

--- multitest_thread.c
+++ multitest_thread.c
@@ -2,5 +2,6 @@
 #include <stdio.h>
 // this file is for multithread
 int start(int target, int* list, int size) {
+    // eyyyyyyyyy wazzup gamerz OwO we about to make some threadz
     printf("owo");
 }
