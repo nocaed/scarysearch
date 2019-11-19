321f26581ec61f8d5dfb4dfd96df58ac6dc90cfe
Thomas C. Brewer <tcb93@ice.cs.rutgers.edu>
2019-11-18 19:01:27 -0500

    Added search function, Added SearchParams struct.

--- multitest_thread.c
+++ multitest_thread.c
@@ -1,7 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+typedef struct _SearchParams
+{
+    int target;
+    int* list;
+    int start;
+    int end;
+} SearchParams;
+
 // this file is for multithread
-int start(int target, int* list, int size) {
-    // eyyyyyyyyy wazzup gamerz OwO we about to make some threadz
-    printf("owo");
+int start(int target, int* list, int size, int threadNum) {
+    pthread_t tid;
+    int i;
+
+    SearchParams* params = (SearchParams*)malloc(sizeof(SearchParams));
+    params -> target = target;
+    params -> list = list;
+    params -> start = start;
+    // TODO: ADD END TO PARAMS
+    
+}
+
+void* search(void* params)
+{
+
 }
