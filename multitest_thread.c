9cbbd1ceac7f02d7b1676a44af9c68e95097a78b
Thomas C. Brewer <tcb93@ice.cs.rutgers.edu>
2019-11-19 12:33:01 -0500

    Finished writing threaded version of search function, still have to finish startSearch

--- multitest_thread.c
+++ multitest_thread.c
@@ -12,19 +12,48 @@ typedef struct _SearchParams
 } SearchParams;
 
 // this file is for multithread
-int start(int target, int* list, int size, int threadNum) {
+int startSearch(int target, int* list, int size, int subArraySize) {
     pthread_t tid;
     int i;
+    // TODO: ADD START TO PARAMS
+    // TODO: ADD END TO PARAMS
+    // error messages
+    if(subArraySize > size)
+    {
+        printf("Error, subarray size cannot be larger than total size.");
+        return -1;
+    }
+    if(subArraySize > 250)
+    {
+        printf("Error, subarray size cannot be larger than 250.");
+        return -1;
+    }
+
 
     SearchParams* params = (SearchParams*)malloc(sizeof(SearchParams));
     params -> target = target;
     params -> list = list;
-    params -> start = start;
-    // TODO: ADD END TO PARAMS
-    
 }
 
+// Searches list of numbers for a target value
 void* search(void* params)
 {
+    SearchParams* seqSearchParams = (SearchParams*)params;
+    int i;
+    void* indexPtr;
+    int* errorNum = (int*)malloc(sizeof(int));
+    errorNum[0] = -1;
 
+    for(i = seqSearchParams -> start; i <= seqSearchParams -> end; i++)
+    {
+        if(seqSearchParams -> list[i] == seqSearchParams -> target)
+        {
+            printf("found\n");
+            indexPtr = seqSearchParams -> list[i];
+            return indexPtr;
+        }
+    }
+
+    indexPtr = (void*)errorNum;
+    return indexPtr;
 }
