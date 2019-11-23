0eb0a40767ce5e8f0953235f599b7d8018d7d56a
--global <codenamedeacon@outlook.com>
2019-11-22 20:36:09 -0500

    removed deprecated functions, commented code

--- searchtest.c
+++ searchtest.c
@@ -8,28 +8,30 @@
 double test(int, int);
 void printTime(double**, int, int);
 void printTestDetails(int);
-int genRandomIntByRange(int upper, int lower);
-double elapsedTimeInMilli(struct timeval* start, struct timeval* end); // gets elapsed time in milliseconds
+double elapsedTimeInMilli(struct timeval* start, struct timeval* end);
 
 int main() {
+    // print if the program has been compiled in process or thread
     printType();
+    // seed the number randomizer
     srand(time(0));
-    const int NUM_TESTS = 13;
-    const int ITERATIONS = 50;
-    double** timeArr = (double**)calloc(NUM_TESTS, sizeof(double*));
-    int i;
-    int j;
-    double standardDeviation = 0.0;
-    double max;
-    double min;
+    const int NUM_TESTS = 13; // number of tests to be run
+    const int ITERATIONS = 50; // number of times each test will be run
+    double** timeArr = (double**)calloc(NUM_TESTS, sizeof(double*)); // 2d array of elapsed time values in ms for each test case
+    int i; // general/outer loop counter
+    int j; // inner loop counter
+    double standardDeviation = 0.0; // holds standard deviation of a test case
+    double max; // holds the maximum runtime of a test case
+    double min; // holds the minimum runtime of a test case
+    // allocate memory for each iteration of a test case
     for(i = 0; i < NUM_TESTS; i++) {
         timeArr[i] = (double*)calloc(ITERATIONS, sizeof(double));
     }
-    double avg = 0.0;
+    double avg = 0.0; // holds the mean runtime for each test case
 
-    // Testing
+    // Running test cases
     for(i = 0; i < ITERATIONS; i++) {
-        // first batch
+        // first batch, vary array sizes
         timeArr[0][i] = test(250, 250);
         timeArr[1][i] = test(500, 250);
         timeArr[2][i] = test(1000, 250);
@@ -38,7 +40,7 @@ int main() {
         timeArr[5][i] = test(8000, 250);
         timeArr[6][i] = test(16000, 250);
         timeArr[7][i] = test(20000, 250);
-        // second batch
+        // second batch, vary subarray sizes
         timeArr[8][i] = test(2000, 16);
         timeArr[9][i] = test(2000, 32);
         timeArr[10][i] = test(2000, 64);
@@ -46,6 +48,7 @@ int main() {
         timeArr[12][i] = test(2000, 250);
     }
 
+    // Print specific details for each test case
     for(i = 0; i < NUM_TESTS; i++) {
         printf("Test Case %d:\n", i+1);
         printf("Test Parameters: ");
@@ -53,13 +56,16 @@ int main() {
         printTime(timeArr, i, ITERATIONS);
         printf("\n");
     }
-    // calculate statistics
+    // Calculate statistics for each test case
     for(i = 0; i < NUM_TESTS; i++) {
+        // Calculate min/max and get sum of runtimes
         for(j = 0; j < ITERATIONS; j++) {
+            // if this is the first runtime, initialize min and max
             if(j == 0) {
                 min = timeArr[i][j];
                 max = timeArr[i][j];
             }
+            // otherwise, check for new min/max values
             else {
                 if(timeArr[i][j] > max) {
                     max = timeArr[i][j];
@@ -68,22 +74,30 @@ int main() {
                     min = timeArr[i][j];
                 }
             }
+            // add to the sum of runtimes
             avg += timeArr[i][j];
         }
+        // output the min/max runtimes
         printf("The minimum runtime for test case %d was %f ms.\n", i+1, min);
         printf("The maximum runtime for test case %d was %f ms.\n", i+1, max);
+        // calculate the average
         avg /= ITERATIONS;
+        // output the average
         printf("The average runtime for test case %d was %f ms.\n", i+1, avg);
+        // calculate the standard deviation
         for(j = 0; j < ITERATIONS; j++) {
             standardDeviation += pow(timeArr[i][j] - avg, 2.0);
         }
         standardDeviation /= ITERATIONS;
         standardDeviation = sqrt(standardDeviation);
+        // output the standard deviation
         printf("The standard deviation for test case %d was %f ms.\n\n", i+1, standardDeviation);
+        // reset average and stdev
         avg = 0.0;
         standardDeviation = 0.0;
     }
     
+    // free the memory taken up by the 2d array of runtimes
     for(i = 0; i < NUM_TESTS; i++) {
         free(timeArr[i]);
     }
@@ -92,22 +106,31 @@ int main() {
     return 0;
 }
 
+// generate a list of {size} bytes
 int* generateList(int size) {
     int i;
+    // allocate memory for the list of numbers
     int* list = (int*) malloc(sizeof(int) * size);
+    // assign values to each index
     for(i = 0; i < size; i++) {
         list[i] = i + 1;
     }
+    // return the list
     return list;
 }
 
+// shuffle a list of values
 void shuffleList(int* list, int size) {
     int i;
+    // the lowest value that can be randomly generated
     int lower = 0;
+    // the highest value that can be randomly generated
     int upper = size - 1;
+    // loop that shuffles the list at every index
     for(i = 0; i < size; i++) {
-        int rand1 = rand() % (upper - lower + 1);
-        int rand2 = rand() % (upper - lower + 1);
+        // randomly choose indexes to swap
+        int rand1 = rand() % (upper - lower + 1) + lower;
+        int rand2 = rand() % (upper - lower + 1) + lower;
 
         int temp;
         // the swap
@@ -117,44 +140,33 @@ void shuffleList(int* list, int size) {
     }
 }
 
-void printList(int* list, int size) {
-    int i;
-    for(i = 0; i < size; i++) {
-        printf("%d, ", list[i]);
-    }
-    printf("\n");
-}
-
-void swapTarget(int *list, int size, int indx) {
-    int lower = 0;
-    int upper = size - 1;
-    int temp;
-    int random = rand() % (upper - lower + 1);
-    temp = list[indx];
-    list[indx] = list[random];
-    list[random] = temp;
-}
-
+// runs a search with an arraySize and a subArraySize
 double test(int arraySize, int subArraySize) {
-    struct timeval start, end;
-    int target;
-    int index;
-    int* list;
+    struct timeval start, end; // hold a time snapshot
+    int target; // the value to find
+    int index; // the index that the target is at
+    int* list; // the list to search
 
+    // take snapshot of time of search start
     gettimeofday(&start, NULL);
+    // set target
     target = 5;
+    // generate a list of arraySize bytes
     list = generateList(arraySize);
+    // shuffle the list
     shuffleList(list, arraySize);
+    // search for the target
     index = search(target, list, arraySize, subArraySize);
+    // error checking
     if(index == -1) {
         printf("Target (%d) was not found.\n", target);
     }
-    else {
-    //    printf("Target (%d) has been found at index %d.\n", target, index);
-    }
+    // free the list
     free(list);
+    // snapshot of runtime end
     gettimeofday(&end, NULL);
 
+    // return the elapsed time
     return elapsedTimeInMilli(&start, &end);
 }
 
@@ -170,12 +182,7 @@ double elapsedTimeInMilli(struct timeval* start, struct timeval* end) {
     return meantime;
 }
 
-int genRandomIntByRange(int lower, int upper) {
-    int randNum = rand() % (upper - lower + 1) + lower;
-    
-    return randNum;
-}
-
+// print the runtime for every test case iteration
 void printTime(double** timeArr, int testNum, int ITERATIONS) {
     int i, j;
     for(i = 0; i < ITERATIONS; i++) {
@@ -188,6 +195,7 @@ void printTime(double** timeArr, int testNum, int ITERATIONS) {
     }
 }
 
+// print the parameters of the test case
 void printTestDetails(int testNum) {
     int arrSize;
     int subArrSize;
